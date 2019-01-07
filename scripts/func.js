e62bda25fa42957420cbd001e655e7555ae43a7b
yiheyue <rightisland@qq.com>
2019-01-07 17:03:10 +0800

    Add new content

--- scripts/func.js
+++ scripts/func.js
@@ -21,3 +21,42 @@ funcNameD(); // ERROR: funcNameD is not defined
 
 console.log(funcNameA); // [Function: funcNameA]
 console.log(funcNameA()); // AAA 23
+
+// Set the default value of parameter
+// 1
+function printABC(a = 1, b = -1, c = 0) {
+  console.log(a, b, c);
+}
+// 2
+function printDEF(d, e, f) {
+  if (d === undefined) {
+    d = 1;
+  }
+  if (e === undefined) {
+    e = -1;
+  }
+  if (f === undefined) {
+    f = 0;
+  }
+  console.log(d, e, f);
+}
+// 3
+function printGHI(g, h, i) {
+  g = g || 1;
+  h = h || -1;
+  i = i || 0;
+  console.log(g, h, i);
+}
+
+// Function declaration, Function expression, Arrow function
+function doSomething() {
+  console.log("declaration");
+}
+
+let doSomething = function() {
+  console.log("expression");
+};
+
+let doSomething = () => {
+  console.log("arrow");
+};
